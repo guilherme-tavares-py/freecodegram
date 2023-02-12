803081a0a4e1ad63b214fd61706acee58ab17564
Guilherme Tavares Pinheiro <gui.tavares2002.contato@gmail.com>
2023-02-12 09:14:01 -0300

    Update Course

---
+++ resources/views/emails/welcome-mail.blade.php
@@ -0,0 +1,9 @@
+<x-mail::message>
+# Welcome to freeCodeGram
+
+This is a community of fellow developers and we love that you have joined us.
+
+
+All the best,<br>
+Guilherme
+</x-mail::message>
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+<form action='/p' enctype="multipart/form-data" method="POST">
+    @csrf
+    <div class="container">
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="row">
+                    <h1>Add New Post</h1>
+                </div>
+                <div class="row mb-3">
+                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+
+    
+                    <input id="caption" 
+                    name='caption'
+                    type="text" 
+                    class="form-control @error('caption') is-invalid @enderror" 
+                    value="{{ old('caption') }}" 
+                    autocomplete="caption" autofocus>
+
+                    @error('caption')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="row mb-3">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    <input type="file" class='form-control' id="image" name="image">
+            
+                    @error('image')
+    
+                        <strong>{{ $message }}</strong>
+                      
+                    @enderror 
+                
+                <div>
+            </div>
+        </div>
+        <div class="row pt-4">
+            <button class="btn btn-primary">Add New Post</button>
+        </div>
+    
+    </div>
+</form>
+@endsection
